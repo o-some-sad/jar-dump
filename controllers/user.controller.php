17f9e7c6bb71671dfd002ac146becb8042a80991
ilawy <next.mohammed.amr@gmail.com>
2025-04-10 16:21:06 +0000

    feat: enhance user retrieval with total count for pagination

--- controllers/user.controller.php
+++ controllers/user.controller.php
@@ -8,12 +8,18 @@ class UserController{
     static public function getAllUsers(int $offset = 0, int $limit = 10){
         try{
             $pdo = createPDO();
-            $stmt = $pdo->prepare("select user_id, name, role, profile_picture from users order by created_at limit :offset, :limit");
-            $stmt->bindParam("offset", $offset, PDO::PARAM_INT);
-            $stmt->bindParam("limit", $limit, PDO::PARAM_INT);
-            $stmt->execute();
-            dd($stmt->fetchAll(PDO::FETCH_ASSOC));
+            $usersStmt = $pdo->prepare("select user_id, name, role, profile_picture from users order by created_at limit :offset, :limit");
+            $usersStmt->bindParam("offset", $offset, PDO::PARAM_INT);
+            $usersStmt->bindParam("limit", $limit, PDO::PARAM_INT);
+            $usersStmt->execute();
+            $data = ($usersStmt->fetchAll(PDO::FETCH_ASSOC));
+
+            $totalStmt = $pdo->prepare("select count(*) as total from users order by user_id");
+            $totalStmt->execute();
+            $total = $totalStmt->fetch(PDO::FETCH_ASSOC)['total'];
+            
             $pdo = null;
+            return compact("data", "total");
         }
         catch(PDOException $e){
             //TODO: handle this
