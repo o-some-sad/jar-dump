029c68de11343d9078c55c4e677a6cb81580dcec
JanaHazem12 <jana.hazem.abbass@gmail.com>
2025-04-15 00:11:47 +0200

    Modify order controller

--- controllers/order.controller.php
+++ controllers/order.controller.php
@@ -19,10 +19,31 @@ class OrderController{
     public function getOrderByDate($DateFrom, $DateTo){
         try{
         $pdo = createPDO();
-        $filterOrdersStmt = $pdo->prepare("select * from orders where created_at BETWEEN {$DateFrom} AND {$DateTo}
+        $filterOrdersStmt = $pdo->prepare("
+        select * 
+        from orders 
+        where created_at BETWEEN {$DateFrom} AND {$DateTo}
         ");
         $filterOrdersStmt->execute();
         $data = ($filterOrdersStmt->fetchAll(PDO::FETCH_ASSOC));
+        print_r($data);
+        $pdo = null;
+        return compact("data");
+        }
+        catch(PDOException $e){
+            dd($e);
+        }
+    }
+    public function getOrderContent(){
+        try{
+        $pdo = createPDO();
+        $displayOrderStmt = $pdo->prepare("
+        select product_id, order_id, quantity from order_items o
+        INNER JOIN products p
+        ON o.product_id = p.product_id
+        WHERE o.order_id = :order_id;");
+        $displayOrderStmt->execute();
+        $data = ($displayOrderStmt->fetchAll(PDO::FETCH_ASSOC));
         print_r($data);
         $pdo = null;
         return compact("data");
